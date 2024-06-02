12bb8c0f1eac512823f866aca3b61d5fce9c656f
Tomoki Hirota <pinkpenguin.tm@gmail.com>
2024-06-02 19:36:08 +0900

    動くようになった

--- my_codes/database_setting.py
+++ my_codes/database_setting.py
@@ -8,7 +8,6 @@ DATABASE = 'sqlite:///data/user_all_post.db'
 # Engine の作成
 Engine = create_engine(
   DATABASE,
-  encoding="utf-8",
   echo=False
 )
 Base = declarative_base()
@@ -22,4 +21,3 @@ session = Session(
 
 # modelで使用する
 Base = declarative_base()
-Base.query = session.query_property()
